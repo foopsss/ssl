156b72f8a0b0b22371b2f01cb0adfe0746e7486e
Fernández Santiago Miguel <166953625+fernandezsantiagomiguel@users.noreply.github.com>
2026-06-23 23:11:29 -0300

    Update parser.py

--- Parser/parser.py
+++ Parser/parser.py
@@ -107,6 +107,7 @@ def p_condicion_temperatura(p):
     '''condicion : OP_NEGACION VALOR_TEMP_OBJ identificador OP_COMPARADOR VALOR_TEMP_ACT
                  | OP_NEGACION VALOR_TEMP_OBJ identificador OP_COMPARADOR VALOR_TEMP_ACT contcondicion
                  | OP_NEGACION VALOR_TEMP_OBJ OP_COMPARADOR VALOR_TEMP_ACT contcondicion
+                 | OP_NEGACION VALOR_TEMP_OBJ OP_COMPARADOR VALOR_TEMP_ACT
                  | VALOR_TEMP_OBJ identificador OP_COMPARADOR VALOR_TEMP_ACT contcondicion
                  | VALOR_TEMP_OBJ identificador OP_COMPARADOR VALOR_TEMP_ACT
                  | VALOR_TEMP_OBJ OP_COMPARADOR VALOR_TEMP_ACT contcondicion
@@ -116,6 +117,7 @@ def p_condicion_humedad(p):
     ''' condicion : OP_NEGACION SENSOR_HUMEDAD identifacor OP_COMPARADOR percent
                   | OP_NEGACION SENSOR_HUMEDAD OP_COMPARADOR percent contcondicion
                   | OP_NEGACION SENSOR_HUMEDAD OP_COMPARADOR percent
+                  | OP_NEGACION SENSOR_HUMEDAD identificador OP_COMPARADOR percent contcondicion
                   | SENSOR_HUMEDAD identificador OP_COMPARADOR percent contcondicion
                   | SENSOR_HUMEDAD identificador OP_COMPARADOR percent
                   | SENSOR_HUMEDAD OP_COMPARADOR percent contcondicion
@@ -125,6 +127,7 @@ def p_condicion_luz(p):
     ''' condicion : OP_NEGACION SENSOR_LUZ identificador OP_COMPARADOR iluminancia contcondicion
                   | OP_NEGACION SENSOR_LUZ OP_COMPARADOR iluminancia contcondicion
                   | OP_NEGACION SENSOR_LUZ OP_COMPARADOR iluminancia
+                  | OP_NEGACION SENSOR_LUZ identificador OP_COMPARADOR iluminancia
                   | SENSOR_LUZ identificador OP_COMPARADOR iluminancia contcondicion
                   | SENSOR_LUZ identificador OP_COMPARADOR iluminancia
                   | SENSOR_LUZ OP_COMPARADOR iluminancia contcondicion
